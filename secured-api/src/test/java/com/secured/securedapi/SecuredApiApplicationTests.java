009751339dcdae8f871752f01c4d2be26d614103
sflankkila <simonflankkila@gmail.com>
2022-10-01 18:04:52 +0300

    Created Spring boot project with Spring Initializr.

---
+++ secured-api/src/test/java/com/secured/securedapi/SecuredApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secured.securedapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuredApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
